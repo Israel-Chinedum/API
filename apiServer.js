18287297cd272503d9897ce35c6affa71a3cc718
Favour Israel <fastarfavour@gmail.com>
2024-10-17 23:27:34 +0000

    updated the api

--- apiServer.js
+++ apiServer.js
@@ -7,18 +7,32 @@ const port = process.env.PORT || 2500;
 app.use(cors());
 app.use(express.json());
 
-//GET REQUEST HADLERS 
-app.get('', (req, res) => {
-    res.json({
-        FirstName: 'Daniel',
-        MiddleName: 'Mazi',
-        LastName: 'Abraham',
-        Email: 'abarahamdaniel@gmail.com',
-        PhoneNumber: '09163383511',
-        ContactAddress: 'Round about',
-        UserName: 'MaziDaniel',
-        Password: '123456'
-    });
+//POST REQUEST HADLERS 
+app.post('', (req, res) => {
+    if(Object.keys(req.body).length === 0){
+        res.status(400).json({error: 'Request body is empty'});
+    } else{
+        res.json({
+            FirstName: 'Daniel',
+            MiddleName: 'Mazi',
+            LastName: 'Abraham',
+            Email: `${req.body.Email}`,
+            PhoneNumber: '09163383511',
+            ContactAddress: 'Round about',
+            UserName: 'MaziDaniel',
+            Password: '123456'
+        });
+        console.log(req.body);
+    }
+});
+
+app.post('/profile/data', (req, res) => {
+    if(Object.keys(req.body).length === 0){
+        res.status(400).json({error: 'Request body is empty'});
+    } else{
+        res.status(200).json({message: 'Data has been recieved!'});
+        console.log(req.body);
+    }
 });
 
 app.listen(port, () => {
